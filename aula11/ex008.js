b48168a3e86602cef7c1ee492f3864e29424c53d
Mr. Tchissing <92308356+mrtchissing@users.noreply.github.com>
2022-03-17 06:21:56 +0100

    Entendo as condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 60
+
+console.log(`A velocidade do seu carro é de ${vel}km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade. Multado!`)
+}
+
+console.log(`Dirija sempre com o cinto de segurança!`)
