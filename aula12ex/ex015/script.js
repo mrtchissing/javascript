cf1d7665f952e07d6fc833b4a0e6c16a57b3f4a9
Mr. Tchissing <92308356+mrtchissing@users.noreply.github.com>
2022-03-21 13:21:03 +0100

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,10 +1,56 @@
 function verificar () {
     var data = new Date()
     var ano = data.getFullYear()
-    var fano = document.getElementById("#txtano")
+    var fano = document.getElementById("txtano")
     var res = document.querySelector("#res")
 
-    if (fano,value.lenght == 0 || Number(fano.value) > 0) {
-        window.alert(`[ERRO] Verfique os dados e novamente`)
-    } 
+    if (Number(fano.value) < 1912 || fano.value > ano) {
+        window.alert(`[ERRO] Verfique os dados e tente novamente!`)
+    } else {
+        var fsex = document.getElementsByName("radsex")
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement("img")
+        img.setAttribute("id", "foto")
+
+        if (fsex[0].checked) {
+            gênero = "Homem"
+
+            if (idade >= 0 && idade < 12) {
+                //foto de criança
+                img.setAttribute("src", "../ex015/img/foto-msc01.png")
+            } else if (idade >= 12 && idade < 18) {
+                //foto de adolescente
+                img.setAttribute("src", "../ex015/img/foto-msc02.png")
+            } else if (idade >= 18 && idade < 60) {
+                //foto de jovem
+                img.setAttribute("src", "../ex015/img/foto-msc03.png")
+            } else {
+                //foto de idoso
+                img.setAttribute("src", "../ex015/img/foto-msc04.png")
+            }
+        } else if (fsex[1].checked) {
+            gênero = "Mulher"
+        }
+
+        if (idade >= 0 && idade < 12) {
+            //foto de criança
+                img.setAttribute("src", "../ex015/img/foto-fem01.png")
+        } else if (idade >= 12 && idade < 18) {
+            //foto de adolescente
+                img.setAttribute("src", "../ex015/img/foto-fem02.png")
+        } else if (idade >= 18 && idade < 60) {
+            //foto de jovem
+                img.setAttribute("src", "../ex015/img/foto-fem03.png")
+        } else {
+            //foto de idoso
+                img.setAttribute("src", "../ex015/img/foto-fem04.png")
+        }
+        /* Para centralizar com Javascript
+        res.style.textAlign = "center"
+        */
+       res.style.fontWeight = "bold"
+       res.innerHTML = `Detectamos o cadastro de ${gênero} de ${idade} anos de idade.`
+       res.appendChild(img)
+    }
 }
